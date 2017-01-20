9edf17b14739a40c374e6b761492b2b6b9432e4c
antonel stefano <stefano.antonel@epfl.ch>
2017-01-20 11:53:00 +0100

    Hiding router with models

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './components/app'
 import VueRouter from 'vue-router'
-import ModelContainer from './components/model-container.vue'
+// import ModelContainer from './components/model-container.vue'
 var configroutes = require('./assets/routes.json')
 
 Vue.use(VueRouter)
@@ -16,13 +16,13 @@ const router = new VueRouter({
       name: configroutes.home },
     { path: configroutes.usecases.home,
       component: App,
-      name: configroutes.usecases.home },
-    { path: configroutes.models,
-      component: ModelContainer,
-      name: configroutes.models },
-    { path: configroutes.usecases.models,
-      component: ModelContainer,
-      name: configroutes.usecases.models }
+      name: configroutes.usecases.home }
+    // { path: configroutes.models,
+    //   component: ModelContainer,
+    //   name: configroutes.models },
+    // { path: configroutes.usecases.models,
+    //   component: ModelContainer,
+    //   name: configroutes.usecases.models }
   ],
   scrollBehavior (to, from, savedPosition) {
     // to scroll to the top every change of route

08882d7b8778650a1315f47a8230f07c65e6c479
antonel stefano <stefano.antonel@epfl.ch>
2017-01-25 14:14:28 +0100

    Change routers for linking with models

--- src/components/uc-container.vue
+++ src/components/uc-container.vue
@@ -1,6 +1,7 @@
 <template>
-   <div class="uc-container">
-      <section v-for="(value, key) in usercases">
+   <div id="uc-container" class="uc-container">
+      <div id="uc-container-title" class="title">Please select a use case</div>
+      <section v-for="(value, key) in usercases" class="uc-item-container">
          <div v-for="uc in value" v-on:click="selected">
             <md-whiteframe md-elevation="2" class="item-sections">
                <uc-item class="uc-item" v-bind:uc="uc" v-bind:categories="categories"></uc-item>
@@ -13,7 +14,7 @@
 <script>
    import ucItem from './uc-item.vue'
    var usercases = require('../assets/usercases.json')
-   // var routes = require('../assets/routes.json')
+   var routes = require('../assets/routes.json')
    export default {
       name: 'ucContainer',
       components: {
@@ -22,7 +23,8 @@
       data () {
          return {
             usercases: usercases[0],
-            categories: usercases[1].categories
+            categories: usercases[1].categories,
+            spa: false
          }
       },
       methods: {
@@ -31,22 +33,50 @@
                elem.classList.remove('selected')
             })
             event.currentTarget.classList.add('selected')
-            // this.$router.push(routes.models)
+            if (this.spa === true) {
+               this.$router.push(routes.usecases.models)
+            }
+         }
+      },
+      mounted () {
+         if (this.$route.fullPath === routes.usecases.usecases) {
+            this.spa = true
+         } else {
+            this.$el.querySelector('#uc-container-title').remove()
+            this.$el.querySelector('.uc-item-container').classList.add('no-title')
          }
       }
    }
 </script>
 
 <style>
-   .uc-container {
+   .uc-container .uc-item-container {
       padding: 10px;
+      margin-top: 50px;
    }
    .item-sections {
       margin-top: 20px;
       padding: 10px;
    }
+   .uc-item-container.no-title {
+      margin-top: 0px;
+   }
    .selected {
       background-color: lightgray;
       transition: background-color 0.5s ease;
+   }
+   .uc-container > .title {
+      box-shadow: 0 2px 5px rgba(0,0,0,.26);
+      position: fixed;
+      text-align: left;
+      color: #fff;
+      background-color: rgba(172, 96, 103, 0.95);
+      padding: 20px;
+      font-size: 20px;
+      font-weight: 600;
+      top: 0;
+      left: 0;
+      width: 100%;
+      z-index: 2;
    }
 </style>

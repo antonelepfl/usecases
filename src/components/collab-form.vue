f4391ec00aa7e6e8e3e8421945d9f507dc4c2086
antonel stefano <stefano.antonel@epfl.ch>
2017-01-31 10:13:54 +0100

    Disable elements and more routings

---
+++ src/components/collab-form.vue
@@ -0,0 +1,73 @@
+<template>
+   <div class="collab-form">
+      <md-whiteframe md-elevation="2">
+         <md-tabs md-fixed md-elevation=2>
+
+            <md-tab id="search" md-label="Search" md-icon="search" class="container-centered">
+               <md-input-container>
+                  <label>Collab Name</label>
+                  <md-input placeholder="Search in your collabs"></md-input>
+               </md-input-container>
+
+               <md-button class="md-raised md-primary button-medium">Search</md-button>
+            </md-tab>
+
+            <md-tab id="create" md-label="Create"  md-icon="create" class="container-centered">
+               <md-input-container>
+                  <label>Collab Name</label>
+                  <md-input placeholder="Create new collab"></md-input>
+               </md-input-container>
+               <md-switch v-model="private" id="priv_pub" name="priv_pub" class="md-primary"></md-switch>
+               <span class="priv_pub">{{private_public}}</span>
+            </md-tab>
+
+         </md-tabs>     
+      </md-whiteframe>
+      
+   </div>
+</template>
+
+<script>
+   export default {
+      name: 'collabForm',
+      data () {
+         return {
+            private: false
+         }
+      },
+      computed: {
+         private_public () {
+            if (this.private) {
+               return 'Private'
+            }
+            return 'Public'
+         }
+      }
+   }
+</script>
+
+<style>
+   .container-centered {
+      display: flex;
+      -ms-flex-direction: column;
+      flex-direction: column;
+      -ms-flex-pack: center;
+      align-items: center;
+   }
+   .collab-form {
+      max-width: 600px;
+      margin: auto;
+   }
+   .button-medium {
+      max-width: 150px;
+   }
+   .collab-form .md-theme-default.md-tabs>.md-tabs-navigation {
+      background-color: rgba(172, 96, 103, 0.95);
+   }
+   .collab-form .container-centered .md-theme-default.md-button.md-raised.button-medium {
+      background-color: rgba(172, 96, 103, 0.95);
+   }
+   .priv_pub {
+      font-size: 20px;
+   }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,27 +1,41 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './components/app'
 import VueRouter from 'vue-router'
-import ModelContainer from './components/model-container.vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
+
 var configroutes = require('./assets/routes.json')
 
+Vue.use(VueMaterial)
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: configroutes.home, // /
-      component: App},
-    { path: configroutes.models, // /models
-      component: ModelContainer},
-    { path: configroutes.usecases.home, // /usecases
-      component: App},
-    { path: configroutes.usecases.usecases, // /usecases/usecases
-      component: App},
-    { path: configroutes.usecases.models, // /usecases/usecases/models
-      component: ModelContainer}
+    { path: configroutes.single_usecases,
+      component: function (resolve) {
+        require(['./components/app.vue'], resolve)
+      },
+      params: {'single': 1}
+    },
+    { path: configroutes.usecases, // /:usecases/
+      component: function (resolve) {
+        require(['./components/app.vue'], resolve)
+      }
+    },
+    { path: configroutes.models.path, // /:usecases/models
+      component: function (resolve) {
+        require(['./components/model-container.vue'], resolve)
+      }
+    },
+    { path: configroutes.collab_form.path, // /:usecases/form
+      component: function (resolve) {
+        require(['./components/collab-form.vue'], resolve)
+      }
+    }
   ],
+  base: '/usecases/',
   scrollBehavior (to, from, savedPosition) {
     // to scroll to the top every change of route
     return { x: 0, y: 0 }
@@ -32,6 +46,11 @@ const router = new VueRouter({
 new Vue({
   el: '#app',
   router,
+  // watch: {
+  //   '$route' (to, from) {
+  //     console.log(to) // react to route changes...
+  //   }
+  // },
   data () {
     return {
     }

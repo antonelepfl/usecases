86788c5d7a0e02454a5bf7099ee246ad7b875b64
antonel stefano <stefano.antonel@epfl.ch>
2017-02-01 17:49:04 +0100

    Change title in form

--- src/components/collab-form.vue
+++ src/components/collab-form.vue
@@ -1,15 +1,19 @@
 <template>
-   <div class="collab-form">      
+   <div class="collab-form">
+     <!--just in case a hidden logout button -->
+      <md-button class="md-raised md-primary button-medium" style="display: none" v-on:click="logout">Logout</md-button>
+      <div v-show="authenticated" class="header">Define in which collab you want to work</div>
       <md-tabs v-if="authenticated" md-fixed class="elevated">
          <md-tab id="search" md-label="Search" md-icon="search" class="container-centered">
             <md-input-container>
                <label>Collab Name</label>
                <md-input placeholder="Search in your collabs" v-model.lazy="searchText"></md-input>
             </md-input-container>
-            <div v-for="collab in collabResults">
-               <div>{{ collab.title }}</div>
+            <div class="collabs-results-container">
+              <div v-for="collab in collabResults" class="collab-result">
+                <a class="nota" href="#">{{ collab.title }}</a>
+              </div>
             </div>
-            <md-button class="md-raised md-primary button-medium">Search</md-button>
          </md-tab>
 
          <md-tab id="create" md-label="Create"  md-icon="create" class="container-centered">
@@ -17,8 +21,10 @@
                <label>Collab Name</label>
                <md-input placeholder="Create new collab"></md-input>
             </md-input-container>
-            <md-switch v-model="private" id="priv_pub" name="priv_pub" class="md-primary priv_pub">{{private_public}}</md-switch>
-            <md-button class="md-raised md-primary button-medium">Create</md-button>
+            <div class="centered">
+              <md-button class="md-raised md-primary button-medium separated ">Create</md-button>
+              <md-switch v-model="private" id="priv_pub" name="priv_pub" class="md-primary priv_pub separated">{{private_public}}</md-switch>
+            </div>
          </md-tab>
       </md-tabs>     
 
@@ -32,7 +38,6 @@
    Vue.use(VueResource)
 
    hbpHello.init({
-      // hbp: '2bc1364d-1039-495b-b51e-608108cbefce' // Replace with your app id
       hbp: '74b1a180-3646-45ac-b53c-ebd905cec418'
    })
 
@@ -56,22 +61,15 @@
          }
       },
       mounted () {
-         // knowing if the token is stil valid
-         var auth = hbpHello.getAuthResponse('hbp')
-         if (auth) {
-            this.authenticated = true
-            Vue.http.headers.common['Authorization'] = 'Bearer ' + auth.access_token;
-         } else {
-           this.login()
-         }
+        this.login()
       },
       methods: {
-         login () {
+         login (displayMethod) {
+            if (displayMethod === undefined) { displayMethod = 'page' }
             var that = this
-            hbpHello.login('hbp', {'display': 'page'}).then(function (event) {
+            hbpHello.login('hbp', {'display': displayMethod, force: false}).then(function (event) {
                if (event.authResponse.access_token) {
-                  that.authenticated = true;
-                  console.info('User Authenticated')
+                  that.saveAuthentication(that, event.authResponse)
                }
             }, function (e) {
                console.debug('Authentication Error', e)
@@ -87,16 +85,23 @@
             });
          },
          searchCollab (param) {
-            if (param.length > 0) {
-               this.$http.get(this.collabAPI + param).then(function (response) {
-                  this.collabResults = response.body.results
-               }, function (responseError) {
-                  console.error(responseError)
-                  // error callback
-               });
-            } else {
-               this.collabResults = []
-            }
+            this.collabResults = []
+            this.$http.get(this.collabAPI + param).then(function (response) {
+              if (param.length > 0) {
+                this.collabResults = response.body.results
+              }
+            }, function (responseError) {
+                if (responseError.status === 401) {
+                  this.collabResults.push = 'Getting your collabs ...'
+                  this.login('none')
+                }
+                console.error(responseError)
+            })
+         },
+         saveAuthentication (context, auth) {
+           context.authenticated = true;
+           console.info('User Authenticated')
+           Vue.http.headers.common['Authorization'] = 'Bearer ' + auth.access_token;
          }
       },
       watch: {
@@ -108,16 +113,11 @@
 </script>
 
 <style>
-   .container-centered {
-      display: flex;
-      -ms-flex-direction: column;
-      flex-direction: column;
-      -ms-flex-pack: center;
-      align-items: center;
+   .collab-form .centered {
+     margin-left: 150px;
    }
-   .collab-form {
-      max-width: 600px;
-      margin: auto;
+   .collab-form .centered .separated {
+     margin-right: 50px;
    }
    .button-medium {
       max-width: 150px;
@@ -125,11 +125,16 @@
    .collab-form .md-theme-default.md-tabs>.md-tabs-navigation {
       background-color: #ac6067;
    }
+   .collab-form .md-tabs-content {
+     width: 550px;
+     margin: 0 auto;
+   }
    .collab-form .md-theme-default.md-button:not([disabled]).md-primary.md-raised.button-medium {
       background-color: #ac6067;
    }
    .collab-form button.md-tab-header.md-active {
       background-color: #884f4d;
+      color: white;
    }
    .collab-form .md-theme-default.md-tabs>.md-tabs-navigation .md-tab-indicator {
       background-color: #1c287e;
@@ -142,12 +147,19 @@
       justify-content: space-between;
       align-items: center;
    }
-   .collab-form .explanation {
-      font-size: 22px;
-      font-family: sans-serif;
+   .collab-form .header {
+      text-align: center;
+      color: #fff;
+      background-color: rgb(172, 96, 103);
+      padding: 25px 20px;
+      font-size: 20px;
+      font-weight: 600;
+      width: 100%;
    }
    .collab-form .elevated {
-      margin-top: 10px;
-      box-shadow: 0 1px 5px rgba(0, 0, 0, 0.2), 0 2px 2px rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12)
+      min-height: 300px;
+   }
+   .collabs-results-container .collab-result > a.nota {
+     color: #ac6067;
    }
 </style>

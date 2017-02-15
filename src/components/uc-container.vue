aa8688ccfc3547afeaaed364461e3f879247c6ad
antonel stefano <stefano.antonel@epfl.ch>
2017-02-15 16:09:13 +0100

    M

--- src/components/uc-container.vue
+++ src/components/uc-container.vue
@@ -39,8 +39,7 @@
                   next = this.$route.path + '/form/' + uc.title.toLowerCase().replace(/\s/g, '')
                }
                next = next.replace('//', '/')
-               console.log(next)
-                this.$router.push({path: next})
+               this.$router.push({path: next})
               //  this.$router.push({name: 'single_usecase', params: {uc: uc}})
             }
          },

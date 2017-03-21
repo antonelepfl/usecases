dab349077dde175b88af0ac33a5f2a3807c7a915
antonel stefano <stefano.antonel@epfl.ch>
2017-03-21 08:20:14 +0100

    Use # route

--- config/index.js
+++ config/index.js
@@ -17,7 +17,7 @@ module.exports = {
     productionGzipExtensions: ['js', 'css']
   },
   firebase: {
-    assetsPublicPath: '/'
+    assetsPublicPath: ''
   },
   ghpages: {
     assetsPublicPath: '/usecases/'

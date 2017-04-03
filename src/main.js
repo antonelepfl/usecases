9777dabbce0c4a8f61b3a6b90d1613a089f22d18
antonel stefano <stefano.antonel@epfl.ch>
2017-04-03 16:48:33 +0200

    Using html5 history

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ Vue.use(VueMaterial)
 Vue.use(VueRouter)
 
 const router = new VueRouter({
+  mode: 'history',
   routes: [
     { path: '/:list_usecases', // display the UC bases on the key of usecases.json
       component: App,

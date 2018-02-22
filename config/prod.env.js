af8c5118cf8deb0aa934f182839c7dc7d4a4ae27
antonel stefano <stefano.antonel@epfl.ch>
2018-02-22 15:46:25 +0100

    Test commit

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,6 +3,6 @@ module.exports = {
   NODE_ENV: '"production"',
   SEND_STATISTICS: true,
   DEV_WEBSITE: process.env.TRAVIS_BRANCH !== 'master',
-  TRAVIS_COMMIT: process.env.TRAVIS_COMMIT
+  TRAVIS_COMMIT: process.env.TRAVIS_COMMIT,
   TRAVIS_COMMIT_MESSAGE: process.env.TRAVIS_COMMIT_MESSAGE
 }

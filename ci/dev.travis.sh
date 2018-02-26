6bdebb22ca0e4a71f415a4e94801af25bc928931
stefanoantonel <stefanoantonel@users.noreply.github.com>
2018-02-26 16:54:51 +0100

    Update dev.travis.sh

--- ci/dev.travis.sh
+++ ci/dev.travis.sh
@@ -1,8 +1,10 @@
 echo "Building for Development"
+
 npm install
 set -e
 npm run lint
 npm run firebase
 npm i firebase-tools
 firebase deploy --public="public" --project="dev-usecases" --token=$FIREBASE_TOKEN
+
 

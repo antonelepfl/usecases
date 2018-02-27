0a319a82780a650c33f8a97cb57b769efc6ff1a3
Stefano Antonel <stefano.antonel@epfl.ch>
2018-02-27 15:33:47 +0100

    Use jenkins ENV for commit

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,7 +2,6 @@ console.log('process.env:', process.env)
 module.exports = {
   NODE_ENV: '"production"',
   SEND_STATISTICS: true,
-  DEV_WEBSITE: process.env.TRAVIS_BRANCH !== 'master',
-  TRAVIS_COMMIT: JSON.stringify(process.env.TRAVIS_COMMIT),
-  TRAVIS_COMMIT_MESSAGE: JSON.stringify(process.env.TRAVIS_COMMIT_MESSAGE)
+  DEV_WEBSITE: process.env.GIT_BRANCH !== 'origin/master',
+  COMMIT_NUMBER: JSON.stringify(process.env.GIT_COMMIT)
 }

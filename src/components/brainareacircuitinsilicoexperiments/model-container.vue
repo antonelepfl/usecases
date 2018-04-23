d94226a266a47caf376bc14ce38d94a975ee7d87
Stefano Antonel <stefano.antonel@epfl.ch>
2018-04-23 09:43:52 +0200

    Fix with brain area

--- src/components/brainareacircuitinsilicoexperiments/model-container.vue
+++ src/components/brainareacircuitinsilicoexperiments/model-container.vue
@@ -13,7 +13,7 @@
 <script>
    import modelItem from './model-item.vue'
    import modelsMixins from 'mixins/models.js'
-   import utils from 'mixins/utils.js'
+   import { compact } from 'mixins/utils.js'
 
    export default {
       name: 'modelContainer',
@@ -33,7 +33,7 @@
               if (model.files) {
                   this.$router.push({name: 'brainarea_form',
                      params: {
-                        'model_name': utils.compact(model.title)
+                        'model_name': compact(model.title)
                      }
                   })
               } else {

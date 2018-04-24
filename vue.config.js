7dc982c92adcff70e43a3608d0eba128070ce004
Stefano Antonel <stefano.antonel@epfl.ch>
2018-04-24 10:43:49 +0200

    Linter fix

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@
 console.log('Base URL:', process.env.BASE_URL)
 console.log('Commit:', process.env.GIT_COMMIT)
 console.log('Branch:', process.env.GIT_BRANCH)
-console.log(JSON.stingify(process.env.GIT_COMMIT))
+console.log(JSON.stringify(process.env.GIT_COMMIT))
 module.exports = {
   baseUrl: process.env.BASE_URL,
   configureWebpack: {

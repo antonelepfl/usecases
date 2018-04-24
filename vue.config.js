e8e98d9d3b817f9b98bcd9aad5ab8af8a8292c0a
Stefano Antonel <stefano.antonel@epfl.ch>
2018-04-24 10:40:09 +0200

    Test Commit Number

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 
 // this BASE_URL will be set in the different jenkins plans
-console.log('Base URL:', process.env.BASE_URL);
+console.log('Base URL:', process.env.BASE_URL)
+console.log('Commit:', process.env.GIT_COMMIT)
+console.log('Branch:', process.env.GIT_BRANCH)
+console.log(JSON.stingify(process.env.GIT_COMMIT))
 module.exports = {
   baseUrl: process.env.BASE_URL,
   configureWebpack: {

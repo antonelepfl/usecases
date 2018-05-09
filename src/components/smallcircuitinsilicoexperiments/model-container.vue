6921d89e6da3887142720277e703a88b9bd1d1e9
Stefano Antonel <stefano.antonel@epfl.ch>
2018-05-09 17:00:01 +0200

    Add Pair Recording app

---
+++ src/components/smallcircuitinsilicoexperiments/model-container.vue
@@ -0,0 +1,51 @@
+<template>
+   <div class="single-cell-insilico-experiments">
+    <div class="title-uc">Please select a model</div>
+    <div class="content-uc">
+      <div v-for="model in models" :key="model.title">
+        <models-list
+          :models="models"
+          v-on:selected="touched">
+        </models-list>
+      </div>
+    </div>
+   </div>
+</template>
+
+<script>
+   import modelList from '@/components/singlecellmodeling/striatal/striatal-list.vue'
+   import createCollab from '@/mixins/createCollab.js'
+   import modelsMixins from '@/mixins/models.js'
+
+   export default {
+    props: ['uc_name'],
+    name: 'modelContainer',
+    components: {
+        'models-list': modelList
+    },
+    data () {
+      return {
+        models: [],
+        ucName: 'configureandrunasmallcircuitusingpreconfiguredhbpmodelanddata'
+      }
+    },
+    mixins: [createCollab],
+    methods: {
+      touched (modelItem) { // open form or pair recording
+        if (modelItem.title === 'Mouse O1') {
+          window.open('https://bsp-mouse-o1.humanbrainproject.eu/', '_blank')
+          return
+        }
+        this.$router.push({name: 'small_circuit_form',
+          params: {
+            'folder_name': modelItem.modelName
+          }
+        })
+      }
+    },
+    created () {
+      this.models = modelsMixins.getModelByUc(this.ucName)
+      document.querySelector('title').innerHTML = 'Small Circuits In Silico Experiments'
+    }
+  }
+</script>

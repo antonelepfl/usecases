acaa88bb7c2984bde0ddd13796043b329b565adb
Stefano Antonel <stefano.antonel@epfl.ch>
2018-08-09 09:39:07 +0200

    Add video tutorial icon

--- src/components/uc/uc-item.vue
+++ src/components/uc/uc-item.vue
@@ -5,16 +5,19 @@
       </div>
       <uc-description class="uc-description" v-bind:uc="uc"></uc-description>
       <uc-tags class="tags" v-bind:uc="uc" v-bind:categories="categories"></uc-tags>
+      <video-tutorial :uc="uc"/>
    </div>
 </template>
 
 <script>
    import ucDescription from './uc-description.vue'
    import ucTags from './uc-tags.vue'
+   import videoTutorial from './video-tutorial.vue'
+
    export default {
       name: 'ucItem',
       components: {
-         ucDescription, ucTags
+         ucDescription, ucTags, videoTutorial
       },
       props: {
          uc: {

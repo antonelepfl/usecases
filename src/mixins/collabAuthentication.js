d5830af2e4cc90202f9370f6030e0ca40d87e7a7
Stefano Antonel <stefano.antonel@epfl.ch>
2018-10-08 09:52:59 +0200

    Add video tutorial and rework Auth

--- src/mixins/collabAuthentication.js
+++ src/mixins/collabAuthentication.js
@@ -1,54 +1,38 @@
-var hbpHello = require('@/assets/hbp.hello.js').hellojs
-var store = require('./store.js').default
 
-// replace this with your collab app id
-hbpHello.init({
-  hbp: '74b1a180-3646-45ac-b53c-ebd905cec418'
-})
+import { JSO } from 'jso';
 
-export default {
-  data () {
-    return {
-      collabAPI: 'https://services.humanbrainproject.eu/collab/v0/'
-    }
-  },
-  methods: {
-    login (displayMethod = 'none', force = false) {
-      return new Promise(function (resolve, reject) {
-        hbpHello.login('hbp', {'display': displayMethod, 'force': force, 'page_uri': window.location.href})
-        .then(function (res) {
-          resolve(res.authResponse.access_token)
-        }, function (e) {
-          console.debug('Login Error', e)
-          reject()
-        });
-      })
-    },
-    logout (force = false) {
-      return new Promise(function (resolve, reject) {
-        hbpHello.logout('hbp', {force: force})
-        .then(function () {
-          console.debug('User Logout OK')
-          resolve()
-        }, function (e) {
-          console.debug('Logout Error', e)
-          reject()
-        });
-      })
-    },
-    renewToken () {
-      this.logout().then(() => {
-        this.login('page', true)
-      })
-    },
-    getAuthResponse () {
-      let session = hbpHello.getAuthResponse('hbp');
-      var currentTime = (new Date()).getTime() / 1000;
-      let valid = session && session.access_token && session.expires > currentTime + 1000;
-      if (valid && !store.state.header.headers) {
-        store.setToken(session.access_token)
-      }
-      return valid
-    }
+import store from './store';
+
+const clientId = '74b1a180-3646-45ac-b53c-ebd905cec418';
+const authUrl = 'https://services.humanbrainproject.eu/oidc/authorize';
+
+const client = new JSO({
+  client_id: clientId,
+  redirect_uri: `${window.location.href}/`,
+  authorization: authUrl,
+  response_type: 'id_token token',
+});
+
+function init() {
+  client.callback();
+
+  const authorization = client.getToken();
+  authorization.then((session) => {
+    store.setToken(session.access_token);
+    redirectToUseCases();
+  });
+
+  return authorization;
+}
+
+function redirectToUseCases() {
+  if (window.location.href.includes('access_token')) {
+    const urlParts = window.location.href.split('/');
+    urlParts.splice(-1,1);
+    window.location.href = urlParts.join('/');
   }
 }
+
+export default {
+  init,
+};

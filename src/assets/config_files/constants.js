01034ab5c9f851b73a70e244eddf3a0f7e84bdb9
Stefano Antonel <stefano.antonel@epfl.ch>
2020-01-14 13:08:56 +0100

    Create Staging switch

---
+++ src/assets/config_files/constants.js
@@ -0,0 +1,8 @@
+
+const githubRepoBase = 'https://github.com/antonelepfl/usecases';
+
+export default {
+  COMMIT_NOTEBOOKS: `${githubRepoBase}/tree/<COMMIT_PLACEHOLDER>/production_notebooks`,
+  COMMIT_URL: `${githubRepoBase}/commit/`,
+  IS_STAGING: 'is_staging',
+};
